724ef869625b0b6ef862074b44d5ca6179ed0e20
Адлет <darkulov_adlet@list.ru>
2026-07-07 14:31:35 +0500

    f

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello, world!")
-
+	fmt.Println("Я сегодня изучил:")
+	fmt.Println("основы языка Go")
+	fmt.Println("Моя любимая игра:")
+	fmt.Println("Мортал комбат 11")
 }
